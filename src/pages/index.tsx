7c76a33e65baffdfcf0424d488e1095412abed3b
Mat Warger <mwarger@gmail.com>
2021-09-11 12:58:37 -0500

    workout list working with TRPC

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,50 @@
-import { Button, Text, View } from 'dripsy';
-import { Link } from 'expo-next-react-navigation';
-import { AnimatePresence, MotiView } from 'moti';
-import React, { useState } from 'react';
+import { Button, Text, View } from 'dripsy'
+import { Link } from 'expo-next-react-navigation'
+import { AnimatePresence, MotiView } from 'moti'
+import React, { useState } from 'react'
+import { Platform } from 'react-native'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { trpc } from '../utils/trpc'
 
 export default function App() {
-	const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false)
+  const workouts = trpc.useQuery(['workout.all'])
 
-	return (
-		<View>
-			<Button onPress={() => setVisible(!visible)} title='Toggle' />
-			<Link style={{ color: 'green' }} routeName='profile'>
-				Click to go to profile
-			</Link>
-			<AnimatePresence>
-				{visible && (
-					<MotiView
-						from={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{
-							opacity: 0,
-						}}
-					>
-						<Text>Stuff</Text>
-					</MotiView>
-				)}
-			</AnimatePresence>
-		</View>
-	);
+  if (workouts.isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
+  const workoutList = workouts.data
+
+  return (
+    <>
+      <View>
+        <Button onPress={() => setVisible(!visible)} title="Toggle" />
+        <Link style={{ color: 'green' }} routeName="profile">
+          Click to go to profile
+        </Link>
+        {workoutList?.map((workout) => (
+          <Text key={workout.id}>{workout.title}</Text>
+        ))}
+        <AnimatePresence>
+          {visible && (
+            <MotiView
+              from={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{
+                opacity: 0,
+              }}
+            >
+              <Text>Stuff</Text>
+            </MotiView>
+          )}
+        </AnimatePresence>
+      </View>
+      {Platform.OS === 'web' && <ReactQueryDevtools initialIsOpen={false} />}
+    </>
+  )
 }
